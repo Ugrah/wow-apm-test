764428d4d173eba742ca95ff74ef9347b22f40d3
Ulrich Grah <grulogdev@gmail.com>
2020-12-26 15:29:35 +0100

    Init wow app test

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class MainController extends Controller
+{
+    //
+    //
+    public $successStatus = 200;
+    /** 
+     * login api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */
+    public function login()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            return response()->json(['success' => $success], $this->successStatus);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+}
